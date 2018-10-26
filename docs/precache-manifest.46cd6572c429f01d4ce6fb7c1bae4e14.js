abdac7ca2d7e0eb7b61809db9ac84f371af5676e
Akhand Mishra <akkimysite@gmail.com>
2018-10-27 00:05:11 +0530

    Production build--test

---
+++ docs/precache-manifest.46cd6572c429f01d4ce6fb7c1bae4e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "728947c92084b3dd5ef4",
+    "url": "/static/js/main.728947c9.chunk.js"
+  },
+  {
+    "revision": "e6832d51165f68d7756e",
+    "url": "/static/js/1.e6832d51.chunk.js"
+  },
+  {
+    "revision": "728947c92084b3dd5ef4",
+    "url": "/static/css/main.7efa62e4.chunk.css"
+  },
+  {
+    "revision": "f3710be083a210a3b6f404d81f0a4489",
+    "url": "/index.html"
+  }
+];
