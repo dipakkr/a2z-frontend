d4c7e8dc4228ab1faca7bce3211f008ddce14eb1
Miteshree Sheth <miteshreesheth@gmail.com>
2018-10-26 19:38:16 -0700

    Responsive heading (#18)
    * updated gitignore
    
    * Responsive heading

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,7 +5,8 @@ export default class Home extends React.Component {
     render() {
         return (
             <div id="home-page-container">
-                <h2>Add the home page here</h2>
+                <h1 className="heading">Explore the ultimate collection</h1>
+                <p className="sub-heading">What's on your mind today? Want to do self-study? Explore new peers?</p>
             </div>
         );
     }
