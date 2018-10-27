da3f84d45c92652500f26ce3675ef39c66e1c84f
Akhand Mishra <akkimysite@gmail.com>
2018-10-27 16:07:10 +0530

    Created Hamburger and Context component

---
+++ src/components/Hamburger/Hamburger.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hamburger.css';
+
+const Hamburger = (props) => {
+    return (
+        <div id="hamburger-menu">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+    );
+}
+
+export default Hamburger;
