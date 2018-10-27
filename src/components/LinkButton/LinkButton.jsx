39f6eea8ac14f731e2b779db7a9317932f2d39d1
Sachin Thakur <thakursachin467@gmail.com>
2018-10-27 23:17:58 +0530

    footer position properly set, LinkButton slightly modified and option… (#21)
    * footer position properly set, LinkButton slightly modified and options to go back to homepage
    
    * updated Links
    
    But I think that redirecting users to a new page for outside link can be a good idea as they can continue interacting with the website.
    
    * Update Layout.css

--- src/components/LinkButton/LinkButton.jsx
+++ src/components/LinkButton/LinkButton.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './LinkButton.css';
 
 const LinkButton = (props) => {
-    
+
     const variableStyles = {
         color: props.color || 'white',
         fontSize: props.fontSize || 'var(--font-size)',
         backgroundColor: props.bgColor || 'var(--light-green)'
     }
-    
+
     return (
         <a href={props.href} style={variableStyles} className="link-button">{props.children}</a>
     );
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = (props) => {
     return (
-        <h2 style={{ color: props.color || '#111' }}>A<span style={{ color: 'var(--theme)' }}><sub>2</sub></span>Z</h2>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+            <h2 style={{ color: props.color || 'var(--black)' }}>A<span style={{ color: 'var(--theme)' }}><sub>2</sub></span>Z</h2>
+        </Link>
     );
 }
 
