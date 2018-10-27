b901cc008ea93a03c778acf35f1522344b212125
Akhand Mishra <akkimysite@gmail.com>
2018-10-27 16:04:58 +0530

    Add navHandler

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,13 +5,26 @@ import Layout from '../../hoc/Layout/Layout';
 import Home from '../Home/Home';
 
 class App extends Component {
+
+  state = {
+    openNav: false,
+  }
+
+  navbarHandler = () => {
+    let current = this.state.openNav;
+    this.setState({
+      openNav: !current
+    });
+    console.log(this.state.openNav);
+  }
+
   render() {
     return (
       <BrowserRouter>  
         <div className="App">
-          <Layout>
-            <Route path="/" exact component={Home} />
-          </Layout>
+            <Layout>
+              <Route path="/" exact component={Home} />
+            </Layout>
         </div>
       </BrowserRouter>
     );
