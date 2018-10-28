4277ee8593db660aaa74debf87295b617f6fe8ce
Akhand Mishra <akkimysite@gmail.com>
2018-10-29 03:46:11 +0530

    Adjustments to landing page

---
+++ build/precache-manifest.79dcffb301207dfa33a629a1e01762f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a43ec67a23a2729fcf75ef460ad76bec",
+    "url": "/a2z-frontend/static/media/stylish.a43ec67a.svg"
+  },
+  {
+    "revision": "3f923b14b63193038de5c0c314e8cb1a",
+    "url": "/a2z-frontend/static/media/back.3f923b14.svg"
+  },
+  {
+    "revision": "25ee61c926988cb6f8b5",
+    "url": "/a2z-frontend/static/js/runtime~main.25ee61c9.js"
+  },
+  {
+    "revision": "f536ff9356f29fccdc6b",
+    "url": "/a2z-frontend/static/js/main.f536ff93.chunk.js"
+  },
+  {
+    "revision": "81afe6d9daa5d071f570",
+    "url": "/a2z-frontend/static/js/1.81afe6d9.chunk.js"
+  },
+  {
+    "revision": "f536ff9356f29fccdc6b",
+    "url": "/a2z-frontend/static/css/main.94073090.chunk.css"
+  },
+  {
+    "revision": "84607b8fa54f88b7bbb28754af46cdd1",
+    "url": "/a2z-frontend/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/a2z-frontend/precache-manifest.42fa8d273f8afed9d05df5390802d5c2.js"
+  "/a2z-frontend/precache-manifest.79dcffb301207dfa33a629a1e01762f7.js"
 );
 
 workbox.clientsClaim();
