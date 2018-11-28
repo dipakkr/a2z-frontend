92f3d9d4841d8799d5bb60f7878fa3f8caf1a2cf
Deepak <raj.deeepak@gmail.com>
2018-11-28 23:43:23 +0530

    renamed footer

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -6,8 +6,6 @@ import Backdrop from '../../components/Backdrop/Backdrop';
 import SlidingNav from '../../components/SlidingNav/SlidingNav';
 import { Consumer } from '../../components/Context/Context';
 import { Link } from 'react-router-dom';
-import github from '../../assets/images/github.png';
-import gitter from '../../assets/images/gitter.png';
 // import profile from '../../assets/images/man.png';
 // import Login from '../../containers/Login/Login';
 // import SignUp from '../../containers/SignUp/SignUp';
@@ -93,48 +91,13 @@ const Layout = (props) => {
 
                     <footer style={isLanding ? landingStyles.footer : null}>
                         <div id="footer-container">
-                            <Logo />
                             <div id="claim">
-                                <p>This project was initiated by Deepak Kumar in 2018</p>
-                                <p>Made with <span style={{ color: "red" }}>♥</span> by the Open Source</p>
+                                {/* <p>Made with <span style={{ color: "red" }}>♥</span></p> */}
+                                <p> © Copyright OneFraction 2018. </p>
                             </div>
-                            <div id="footer-links-container">
-                                <a
-                                    href="https://github.com/dipakkr/A-to-Z-Resources-for-Students"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="footer-link">
-                                    <span><img src={github} alt="" /></span>Github
-                                    </a>
-                                <a
-                                    href="https://gitter.im/a2z-resources"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="footer-link">
-                                    <span><img src={gitter} alt="" /></span>Gitter
-                                    </a>
-                            </div>
-
+                           
                         </div>
                     </footer>
-                    <div className="side-links-container" style={isLanding ? landingStyles.extra : null}>
-                        <div className="side-link">
-                            <a
-                                href="https://github.com/dipakkr/A-to-Z-Resources-for-Students"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                <img src={github} alt="" />
-                            </a>
-                        </div>
-                        <div className="side-link">
-                            <a
-                                href="https://gitter.im/a2z-resources"
-                                target="_blank"
-                                rel="noopener noreferrer">
-                                <img src={gitter} alt="" />
-                            </a>
-                        </div>
-                    </div>
                 </>
             }}
         </Consumer>
