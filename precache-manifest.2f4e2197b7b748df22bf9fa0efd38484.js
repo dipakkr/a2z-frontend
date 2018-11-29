00d4375db0e59ec4080bb7c5633cd357c6670254
Akhand Mishra <akkimysite@gmail.com>
2018-11-29 15:56:44 +0530

    Updates

---
+++ precache-manifest.2f4e2197b7b748df22bf9fa0efd38484.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d73e644bdcfdee9b1198c41a0cfef81",
+    "url": "/a2z-frontend/static/media/vector.0d73e644.png"
+  },
+  {
+    "revision": "429d5f8f7d0d814a1e6423dc717b28b4",
+    "url": "/a2z-frontend/static/media/back.429d5f8f.png"
+  },
+  {
+    "revision": "c8af58dc175104db94c702efddc9abfb",
+    "url": "/a2z-frontend/static/media/RageItalic.c8af58dc.eot"
+  },
+  {
+    "revision": "8d6ca60ffb3af211550bd638e5d28241",
+    "url": "/a2z-frontend/static/media/RageItalic.8d6ca60f.ttf"
+  },
+  {
+    "revision": "6f7b12de908cdda0e4a3557a374c20f1",
+    "url": "/a2z-frontend/static/media/RageItalic.6f7b12de.woff"
+  },
+  {
+    "revision": "25ee61c926988cb6f8b5",
+    "url": "/a2z-frontend/static/js/runtime~main.25ee61c9.js"
+  },
+  {
+    "revision": "32b8b62630e149bdf79b",
+    "url": "/a2z-frontend/static/js/main.32b8b626.chunk.js"
+  },
+  {
+    "revision": "180cab249b4eff75c727",
+    "url": "/a2z-frontend/static/js/1.180cab24.chunk.js"
+  },
+  {
+    "revision": "32b8b62630e149bdf79b",
+    "url": "/a2z-frontend/static/css/main.6eac90e8.chunk.css"
+  },
+  {
+    "revision": "b3211723f67db15338a5d0889aa24097",
+    "url": "/a2z-frontend/index.html"
+  }
+];
