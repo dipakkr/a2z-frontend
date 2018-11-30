e4171ff626617f82cb531dd95cd94a8cf60d5dd1
Akhand Mishra <akkimysite@gmail.com>
2018-11-30 21:52:27 +0530

    Minor Tweaks

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,12 +4,14 @@ import './About.css';
 const About = (props) => {
     return (
         <div className="about-page-container">
-            <h1>FrontBench <span> | An initiative by Open Source for youths</span></h1>
-            <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-            <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-            <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-            <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-            <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+            <div className="about-container">
+                <h1>FrontBench <span> | An initiative by Open Source for youths</span></h1>
+                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+            </div>
         </div>
     );
 }
