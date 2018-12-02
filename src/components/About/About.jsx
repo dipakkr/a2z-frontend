e70ec62caf9d86207acacef878152f0b3f07ed48
Deepak <raj.deeepak@gmail.com>
2018-12-02 12:03:23 +0530

    added about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,32 @@ const About = (props) => {
     return (
         <div className="about-page-container">
             <div className="about-container">
-                <h1>FrontBench <span> | An initiative by Open Source for youths</span></h1>
-                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
-                <p className="about-p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad alias ipsa labore iste omnis. Qui, quo non dolorum eaque deleniti consequuntur excepturi est cumque deserunt ullam sit omnis eius asperiores.</p>
+                <h1>FrontBench <span> | Empowering Youths</span></h1>
+                <p className="about-p">
+                Welcome
+                 We at FrontBench are dedicated to empower every youth of the country. We believe that quality education and a mentor in life is key to achieve every single thing in life.
+                </p>
+
+                {/* <p className="about-p">
+                <u><b>The Problem</b> </u><br/><br/> 
+
+                Very often, people from TIER 3 institutions complain that they pay an exhaustive price for the chance they had missed once years back or they didn't have any guidance in college. 
+                The problem isn't that they aren't hard working or sincere but the real problem is in the fact that they aren't aware of the opportunities available to them.
+                <br/> <br/> 
+                How to begin? Where to begin? Be it for anything. Though Internet is available everywhere, the thing is that, at least they should have heard of the word before they can even google it.
+
+                <br/><br/><u><b>Our Solution</b> </u><br/><br/> 
+                We aim to provide every possible things a newbie shoould do in his/her field to get most out of it. Whether you are a college student or a working professional looking for resources to learn a new coding language
+                ,meet new people in your community or searching for global conferences, hackathons, competitions, bootcamps or mentorship/guidance. You can find everything at FrontBench.
+
+                <br/><br/> Come Join us and Never regret later that you didn't have any guidance. <br/><br/> 
+
+                <br/><br/>  <br/><br/> 
+
+                </p> */}
+
+
+             
             </div>
         </div>
     );
