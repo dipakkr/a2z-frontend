d1c5596e2cccb752489119989f16c2ae0d55c1ae
Deepak <raj.deeepak@gmail.com>
2018-12-09 04:38:32 +0530

    update in campus connect

--- src/components/CampusStar/CampusStar.jsx
+++ src/components/CampusStar/CampusStar.jsx
@@ -7,7 +7,7 @@ const CampusStar = (props) => {
             <div className="contact-container">
                 <h1> Campus Connect Program </h1>
                 <br/>
-                <p className="about-p">Want to create a better ecosystem at your Campus ? Be the part of change you want to see in world. Register for our Campus Connect Program. </p>
+                <p className="about-p1">Want to create a better ecosystem at your Campus ? Be the part of change you want to see in world. Register for our Campus Connect Program. </p>
                
                 <iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSfRfFgMcdUexKsq2yWKZbGDBLH_Zb1p-auIVJ3v9fictnuarQ/viewform?embedded=true"></iframe>
 
