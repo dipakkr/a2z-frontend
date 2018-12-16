312b7cbbc86262d70f702ffc3e08cb135d85910b
dio <dhrubesh97@gmail.com>
2018-12-17 02:57:19 +0530

    CSS update

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -5,17 +5,33 @@ const Blog = (props) => {
   return (
     <div className="blog-page-container">
       <div className="blog-container blog-background">
-        <h1 className="blog-h1">Blog </h1>
+        <h1 className="blog-h1">Campus Stories </h1>
       </div>
 
       <div className="blog-list-container">
         <div className="blog-individual">
-          <p className="blog-title">Exploiting Developer Infrastructure Is Ridiculously Easy</p>
-          <p className="blog-author">By <b>Anonymous</b> on <b>15.08.2020</b></p>
+          <div className="blog-img">
+            <img src="https://via.placeholder.com/100 " alt="blog" />
+          </div>
+          <div className="blog-data">
+            <p className="blog-title">Exploiting Developer Infrastructure Is Ridiculously Easy</p>
+            <p className="blog-author-container"> Author:
+              <span className="blog-author">Anonymous</span>
+              <span className="blog-date">15.08.2020</span>
+            </p>
+          </div>
         </div>
         <div className="blog-individual">
-          <p className="blog-title">The tipping point of Persuasive Design</p>
-          <p className="blog-author">By <b>Anonymous</b> on <b>15.08.2020</b></p>
+          <div className="blog-img">
+            <img src="https://via.placeholder.com/100 " alt="blog" />
+          </div>
+          <div className="blog-data">
+            <p className="blog-title">A Study Plan To Cure JavaScript Fatigue</p>
+            <p className="blog-author-container"> Author:
+              <span className="blog-author">Anonymous</span>
+              <span className="blog-date">15.08.2020</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
