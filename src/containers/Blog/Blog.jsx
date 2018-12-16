55933c4dc86df84e10696f7951939dcd698adb93
dio <dhrubesh97@gmail.com>
2018-12-16 15:13:33 +0530

    minor additions

--- src/containers/Blog/Blog.jsx
+++ src/containers/Blog/Blog.jsx
@@ -10,10 +10,12 @@ const Blog = (props) => {
 
       <div className="blog-list-container">
         <div className="blog-individual">
-          <p>Title</p>
+          <p className="blog-title">Exploiting Developer Infrastructure Is Ridiculously Easy</p>
+          <p className="blog-author">By <b>Anonymous</b> on <b>15.08.2020</b></p>
         </div>
         <div className="blog-individual">
-          <p>Title</p>
+          <p className="blog-title">The tipping point of Persuasive Design</p>
+          <p className="blog-author">By <b>Anonymous</b> on <b>15.08.2020</b></p>
         </div>
       </div>
     </div>
