ef6dfc24e1bccda6d8e25cab1d89437478ce78e7
Deepak <raj.deeepak@gmail.com>
2018-12-17 12:48:39 +0530

    prod build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/a2z-frontend/precache-manifest.1ec136ea86d334786d36ccec75ddc7c5.js"
+  "/a2z-frontend/precache-manifest.dbb5b2f96ec7c7d713546a282557ab67.js"
 );
 
 workbox.clientsClaim();
--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -5,9 +5,9 @@ const ContactUs = (props) => {
     return (
         <div className="contact-page-container">
             <div className="contact-container">
-                <h1> Connect With Us </h1>
+                {/* <h1> Connect With Us </h1>
                 {/* <iframe title="form" src="http://eepurl.com/dP9-05"></iframe> */}
-                <iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSe770z5vA6-YFun8GTz87mXXCoQ_8Mlsu00asWT4-6whBS1yQ/viewform?embedded=true"></iframe>
+                <iframe title="form" src="https://docs.google.com/forms/d/e/1FAIpQLSe770z5vA6-YFun8GTz87mXXCoQ_8Mlsu00asWT4-6whBS1yQ/viewform?embedded=true"></iframe> */}
 
             </div>
         </div>
