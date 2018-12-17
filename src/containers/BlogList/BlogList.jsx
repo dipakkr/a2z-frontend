b2b800d7baa76fe284dd664daf83ef5681571681
dio <dhrubesh97@gmail.com>
2018-12-17 12:13:06 +0530

    converted the functional component to class-based components

--- src/containers/BlogList/BlogList.jsx
+++ src/containers/BlogList/BlogList.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import './BlogList.css';
 import BlogListCard from '../../components/BlogListCard/BlogListCard';
 
-const BlogList = (props) => {
-  return (
-    <div className="blog-page-container">
-      <div className="blog-container blog-background">
-        <h1 className="blog-h1">Campus Stories </h1>
-      </div>
+export default class BlogList extends React.Component {
 
-      <div className="blog-list-container">
-        <BlogListCard />
+  state = {
+  }
+
+  render() {
+    return (
+      <div className="blog-page-container">
+        <div className="blog-container blog-background">
+          <h1 className="blog-h1">Campus Stories </h1>
+        </div>
+        <div className="blog-list-container">
+          <BlogListCard />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default BlogList;
