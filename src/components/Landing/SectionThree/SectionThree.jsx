038c53f8262950a2ae71f276c2d679d1977ed66b
dio <dhrubesh97@gmail.com>
2018-12-21 23:26:14 +0530

    third section Dummy View completed

---
+++ src/components/Landing/SectionThree/SectionThree.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './SectionThree.css';
+
+const SectionThree = (props) => {
+
+  return (
+    <div className="padding-container">
+      <div className="featured"><h3>Featured</h3></div>
+      <div className="features">
+        <div>Company 1</div>
+        <div>Company 2</div>
+        <div>Company 3</div>
+        <div>Company 4</div>
+      </div>
+    </div>
+  );
+}
+
+export default SectionThree;
