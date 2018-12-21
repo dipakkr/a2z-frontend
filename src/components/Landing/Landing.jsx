94aef15f0da0e8994f22de0909a0266f02604313
dio <dhrubesh97@gmail.com>
2018-12-21 15:37:05 +0530

    Removed unused imports

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './Landing.css';
 import back from '../../assets/images/back.png';
-import Test from '../../assets/images/svg1-small.svg';
-import Test2 from '../../assets/images/Svg2-large.svg';
-import black from '../../assets/images/vector.png';
-import side from '../../assets/images/side-drop.png';
+import blackLeft from '../../assets/images/svg1-small.svg';
+import blackRight from '../../assets/images/Svg2-large.svg';
 import { Link } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 
@@ -33,11 +31,9 @@ class Landing extends React.Component {
                         <p>Explore the ultimate collection</p>
                         <div><Link to="/home">Get Started</Link></div>
                     </div>
-                    {/* <img src={black} alt="" className="background-svg-2" onLoad={() => this.setState({ loadingBlack: false })} /> */}
                     <img src={back} alt="" className="background-svg-1" onLoad={() => this.setState({ loadingBack: false })} />
-                    {/* <img src={side} alt="" className="background-svg-3" /> */}
-                    <img src={Test} alt="" className="background-svg-10" onLoad={() => this.setState({ loadingBlack: false })} />
-                    <img src={Test2} alt="" className="background-svg-20" onLoad={() => this.setState({ loadingBlack: false })} />
+                    <img src={blackLeft} alt="" className="background-svg-10" onLoad={() => this.setState({ loadingBlack: false })} />
+                    <img src={blackRight} alt="" className="background-svg-20" onLoad={() => this.setState({ loadingBlack: false })} />
                     <div className="scroll-div">
                         <button className="scroll-btn" title="Click to stay updated"><i className="down"></i></button>
                     </div>
