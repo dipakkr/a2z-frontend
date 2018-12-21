60916d91666a65892d8bb138e270d4fb94f06f00
dio <dhrubesh97@gmail.com>
2018-12-21 18:04:41 +0530

    segregated landing page into sections based components

---
+++ src/components/Landing/SectionOne/SectionOne.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './SectionOne.css';
+import back from '../../../assets/images/back.png';
+import blackLeft from '../../../assets/images/svg1-small.svg';
+import blackRight from '../../../assets/images/Svg2-large.svg';
+import { Link } from 'react-router-dom';
+
+const SectionOne = (props) => {
+
+  return (
+    <>
+      <div id="special-container">
+        <h1>Resources for Students</h1>
+        <p>Explore the ultimate collection</p>
+        <div><Link to="/home">Get Started</Link></div>
+      </div>
+      <img src={back} alt="" className="background-svg-1" onLoad={() => props.stopLoading()} />
+      <img src={blackLeft} alt="" className="background-svg-10" onLoad={() => props.stopLoading()} />
+      <img src={blackRight} alt="" className="background-svg-20" onLoad={() => props.stopLoading()} />
+      <div className="scroll-div">
+        <button className="scroll-btn" title="Click to stay updated"><i className="down"></i></button>
+      </div>
+    </>
+  );
+}
+
+export default SectionOne;
