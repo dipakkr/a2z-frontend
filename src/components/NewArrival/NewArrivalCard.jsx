ac16f96c7891294762ffe56456013adfbb00d46a
Deepak <raj.deeepak@gmail.com>
2019-01-27 13:11:06 +0530

    updated new arrival

--- src/components/NewArrival/NewArrivalCard.jsx
+++ src/components/NewArrival/NewArrivalCard.jsx
@@ -5,15 +5,16 @@ const NewArrivalCard = (props) => {
     return (
        
             <div className="meetups-card">
-                <h3>{props.title}</h3>
+                <h2>{props.title}</h2>
                 <hr/>
-                <span><span className="des-sp">Description:</span> {props.area}</span>
+                <span><span className="des-sp">Description :</span> {props.area}</span>
                 <span><span className="des-sp">Date :</span> {'Best Conference in the world'}</span>
-                <span><span className="des-sp">Place:</span> {'Frankfurt, Germany'}</span>
+                <span><span className="des-sp">Place :</span> {'Frankfurt, Germany'}</span>
+                <span><span className="des-sp">Scholarship :</span> {'Not Applicable'}</span>
                 
                 <span><span className="des-sp">Frontbench Review :</span> {'All students should participate in this conference.'}</span>
                 <a href={props.link} target="_blank" rel="noopener noreferrer">
-                <span><span className="des-sp">Visit URL :</span> {'https://frontbench.xyz/1234'}</span>
+                <span><span className="des-url"> <a href={props.url}> Visit URL </a> </span> </span>
                 </a>
             </div>
         
