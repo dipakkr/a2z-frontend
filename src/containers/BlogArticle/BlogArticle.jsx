35422d1a98decc1b03d330ca80a52498c28191f1
Deepak <raj.deeepak@gmail.com>
2019-02-02 22:12:25 +0530

    blog working

--- src/containers/BlogArticle/BlogArticle.jsx
+++ src/containers/BlogArticle/BlogArticle.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './BlogArticle.css'
 import client from '../../service/client'
+import marked from 'marked'
 
 class BlogArticle extends React.Component { 
     constructor(){
@@ -18,11 +19,14 @@ class BlogArticle extends React.Component {
         }
     }
 
-    render(){
-       
+    getParsedMarkdown(content){
+        return{
+            __html : marked(content, {sanitize : true})
+        }
+    }
 
-      
-        
+    render(){
+         
         if(!this.state.article){
             return <h1> Loading .. </h1>
         }
@@ -55,57 +59,11 @@ class BlogArticle extends React.Component {
                         </li>
                     </ul>
                 </div>
-                <div className={'blog-content'} id='content'>
-                    {this.state.article.fields.content}
-                </div>
+                <div className={'blog-content'} id='content' dangerouslySetInnerHTML={this.getParsedMarkdown(this.state.article.fields.content)}/>
+                
             </>
-            )
-
-
-       
-        
+        )
     }
 }
 
 export  default BlogArticle
-
-// const Content=props=>{
-	
-// 	if(parseInt(props.match.params.id)>0){ //logic to check valid blog id
-// 		return(
-// 		<>
-// 			<h1 className={'blog-heading'}>
-// 				Placement at TCS<br />
-// 				{ `Blog id: ${props.match.params.id}` } {/*`content to be fetched from server with this blog id */}
-// 			</h1>
-
-// 			<div className={'post-heading'}>
-// 				<ul className={'post-heading-ul'}>
-// 					<li>
-// 						<span className={'blog-author'}>
-// 					&nbsp;<span className={'name'}>{'Arisha Tomar'}</span>
-// 						</span>
-// 					</li>
-// 					<li>
-// 						<time>
-// 					posted on&nbsp;<span>{Date()}</span>
-// 						</time>
-// 					</li>
-// 					<li>
-// 						<span className={'blog-shares'}>
-				
-// 							<a href="https://www.facebook.com/sharer/sharer.php?u=#url"><i className={'fab fa-facebook-square'}></i></a>
-// 							<a href={`whatsapp://send?text=${window.location}`}><i className={'fab fa-whatsapp'}></i></a>
-// 							<a href={`https://twitter.com/share?url=${window.location}`}><i className={'fab fa-twitter'}></i></a>
-// 						</span>
-// 					</li>
-// 				</ul>
-// 			</div>
-// 			<div className={'blog-content'} id='content'>
-				
-// 			</div>
-// 		</>
-// 		)
-// 	}
-// }
-// export default Content
